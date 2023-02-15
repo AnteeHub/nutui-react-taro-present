1bc751d92ead235ecec743a0bc1f378da1082070
汤鸿程 <hongchengtang@tuputech.com>
2023-02-15 18:29:18 +0800

    chore: 新增 dialog 组件的示例

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,14 @@
 import { Component, PropsWithChildren } from "react";
-import { Textarea as RawTextarea } from "@tarojs/components";
-import { Button, Cell, TextArea } from "@nutui/nutui-react-taro";
+import { Button, Dialog } from "@nutui/nutui-react-taro";
 import "./index.scss";
 
 class Index extends Component<PropsWithChildren> {
   constructor(props: any) {
     super(props);
+
+    this.state = {
+      visible: false,
+    };
   }
 
   componentDidMount() {}
@@ -21,9 +24,38 @@ class Index extends Component<PropsWithChildren> {
       <div className="nutui-react-demo">
         <div className="index">欢迎使用 NutUI React 开发 Taro 多端项目。</div>
         <div className="index">
-          <TextArea placeholder="nut-ui 带占位符的 textarea" disabled />
-          <TextArea defaultValue="nut-ui 带默认内容的 textarea" disabled />
-          <RawTextarea placeholder="taro 原生的 textarea" disabled />
+          <Dialog
+            title="使用组件弹窗"
+            content="这里是不起作用的"
+            visible={this.state.visible}
+            onOk={() => {
+              this.setState({ visible: false });
+            }}
+            onCancel={() => {
+              this.setState({ visible: false });
+            }}
+          >
+            组件调用对话框，用 `content` 属性是不起作用的
+          </Dialog>
+          <Button
+            onClick={() => {
+              Dialog.alert({
+                title: "使用 Dialog.alert 弹窗",
+                content: "内容",
+              });
+            }}
+          >
+            使用 Dialog.alert 弹窗
+          </Button>
+          <Button
+            onClick={() => {
+              this.setState({
+                visible: !this.state.visible,
+              });
+            }}
+          >
+            使用组件弹窗
+          </Button>
         </div>
       </div>
     );
